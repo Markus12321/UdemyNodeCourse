ff5971bbf73590a1c3b7f2e481893e8a3311f65d
Markus B <Markus1abc@gmail.com>
2019-12-17 23:15:36 +0100

    Added some ting

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,4 +1,30 @@
+const path = require('path')
+
 const express = require('express')
 
+
+
 const app = express()
+
+//app.com is our domain
+//app.com/help
+//app.com/about
+//these are different route
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
+
+
+
+app.get('/weather', (req, res) => {
+    res.send([{
+        Temprature: 27,
+        Humidity: 0.25
+    }])
+})
+
+
+app.listen(3000, () =>{
+    console.log('Server is up on port 3000.')
+})
 
